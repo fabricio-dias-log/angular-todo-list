f6042cbae7cc9267c754a6ca0070822c69e148c5
Fabricio Dias <fabricio270909@gmail.com>
2025-02-13 23:34:56 -0300

    feat: print data and make validation

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { TaskList } from '../../model/task-list';
 
 @Component({
   selector: 'app-todo-list',
   standalone: false,
-  
+
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.css'
 })
 export class TodoListComponent {
+  public taskList: Array<TaskList> = [
+    { task: 'Task 1', checked: true },
+    { task: 'Task 2', checked: false },
+    { task: 'Task 3', checked: true },
+  ];
 
 }
